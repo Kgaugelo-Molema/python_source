8e85ce1a446ff6e2d67cbe822276ae8fe01c9f60
kmolema@gmail.com <kmolema@gmail.com>
2017-02-14 21:10:32 +0200

    Add addBook method to SQLiteLayer file

--- SQLiteLayer.py
+++ SQLiteLayer.py
@@ -4,7 +4,7 @@ from PyQt5 import QtSql, QtGui, QtWidgets
 def createDB():
     print('Creating DB')
     db = QtSql.QSqlDatabase.addDatabase('QSQLITE')
-    db.setDatabaseName('sports.db')
+    db.setDatabaseName('citylibrary.db')
     if not db.open():
         QtGui.QMessageBox.critical(None, QtGui.qApp.tr("Cannot open database"),
                                    QtGui.qApp.tr("Unable to establish a database connection.\n"
@@ -15,17 +15,40 @@ def createDB():
                                    QtGui.QMessageBox.Cancel)
         return False
     query = QtSql.QSqlQuery()
-    createSportsDB(query)
+    createLibraryDB(query)
     return True
 
-def createSportsDB(query):
-    query.exec_("create table sportsmen(id int primary key, "
-                "firstname varchar(20), lastname varchar(20))")
-    query.exec_("insert into sportsmen values(101, 'Roger', 'Federer')")
-    query.exec_("insert into sportsmen values(102, 'Christiano', 'Ronaldo')")
-    query.exec_("insert into sportsmen values(103, 'Ussain', 'Bolt')")
-    query.exec_("insert into sportsmen values(104, 'Sachin', 'Tendulkar')")
-    query.exec_("insert into sportsmen values(105, 'Saina', 'Nehwal')")
+def createLibraryDB(query):
+    #Inventory
+    query.exec_("create table inventory(id int primary key, "
+                "type varchar(10), datepurchased datetime)")
+    # query.exec_("insert into sportsmen values(102, 'Christiano', 'Ronaldo')")
+    # query.exec_("insert into sportsmen values(103, 'Ussain', 'Bolt')")
+    # query.exec_("insert into sportsmen values(104, 'Sachin', 'Tendulkar')")
+    # query.exec_("insert into sportsmen values(105, 'Saina', 'Nehwal')")
+
+    # Book
+    query.exec_("create table book(title varchar(100), "
+                "isbn varchar(50), author varchar(100), "
+                "yearpublished int, quantity int, inventoryid int)")
+
+def addBook(title, isbn, author, year, qty):
+    print('Adding book to DB')
+    db = QtSql.QSqlDatabase.addDatabase('QSQLITE')
+    db.setDatabaseName('citylibrary.db')
+    if not db.open():
+        QtGui.QMessageBox.critical(None, QtGui.qApp.tr("Cannot open database"),
+                                   QtGui.qApp.tr("Unable to establish a database connection.\n"
+                                                 "Click Cancel to exit."),
+                                   QtGui.QMessageBox.Cancel)
+        return False
+    #query = QtSql.QSqlQuery()
+
+    # model = QtSql.QSqlTableModel()
+    # model.setTable('inventory')
+
+    #query.exec_("insert into inventory values(101, 'Book', '14 Feb 2017')")
+    return True
 
 if __name__ == '__main__':
     import sys
